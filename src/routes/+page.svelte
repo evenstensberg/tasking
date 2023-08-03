d83e03f0ebc934f2ac5564780b1ecb9183c85e49
Even Stensberg <evenstensberg@gmail.com>
2023-08-03 22:42:34 +0200

    feat: implementation

---
+++ src/routes/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+	const tasks = [
+		{
+			priority: 1.2,
+			description: 'Go to work at 10:00 and finish 18:00'
+		},
+		{
+			priority: 4.2,
+			description: 'Exercise 30 minutes'
+		},
+		{
+			priority: 2.9,
+			description: 'Read a book'
+		},
+		{
+			priority: 5.0,
+			description: 'Morning coffee'
+		}
+	];
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="main">
+	{#each tasks as task}
+		<div class="task-container">
+			<div class="priority-container">
+				<span class="priority-text">{task.priority}</span>
+			</div>
+			<div class="description-container">
+				<span>{task.description}</span>
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.priority-text {
+		font-size: 60px;
+		color: white;
+	}
+	.priority-container {
+		height: auto;
+		width: 10%;
+		display: grid;
+		justify-content: center;
+		align-items: center;
+		background-color: rgb(94, 108, 129);
+		border-radius: 1em;
+	}
+	.description-container {
+		border: 2px solid black;
+		height: auto;
+		width: 100%;
+		display: grid;
+		justify-content: start;
+		align-items: center;
+		margin-left: 10px;
+		color: white;
+		background-color: rgb(94, 108, 129);
+		border-radius: 1em;
+	}
+	.main {
+		width: 97%;
+		height: auto;
+		margin-left: 3em;
+	}
+	.task-container {
+		display: flex;
+		width: 100%;
+		height: 10em;
+		margin-top: 2em;
+	}
+	.description-container > span {
+		padding: 2em;
+		font-size: 30px;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+</script>
+
+<header>
+	<div class="tasking-container">
+		<p class="tasking-name">TASKING</p>
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.tasking-container {
+		margin-top: 1em;
+		border: 2px solid white;
+		width: 20em;
+		height: 10em;
+		border-top-right-radius: 5em;
+		border-bottom-right-radius: 5em;
+		background-color: white;
+	}
+
+	.tasking-name {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+		font-size: 50px;
+		color: red;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style>
